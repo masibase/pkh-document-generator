89c7e70f749d12ad667634de7f76b145e2b3ab35
Z User <z@container>
2026-07-18 23:13:44 +0000

    chore: remove sandbox infrastructure from tracked files
    - Remove .env (local DB path, use .env.example instead)
    - Remove .zscripts/ (sandbox dev/build scripts)
    - Remove Caddyfile (sandbox gateway config)
    - Remove start-dev.sh (sandbox launcher)
    - Remove download/ (runtime output dir)
    - Update .gitignore to prevent re-tracking

--- start-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting next dev..."
-  exec node /home/z/my-project/node_modules/.bin/next dev -p 3000 > /home/z/my-project/dev.log 2>&1
-  echo "[$(date)] next dev exited, restarting in 2s..."
-  sleep 2
-done
